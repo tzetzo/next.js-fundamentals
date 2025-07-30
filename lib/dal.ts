5d5a82b2b2351dad70cf4b1cf52d861a07d12e18
Tzvetan Marinov <tmarinov@stamus-networks.com>
2025-07-30 10:27:21 +0300

    server actions moved outside the app folder

--- lib/dal.ts
+++ lib/dal.ts
@@ -1,6 +1,51 @@
 import { db } from '@/db'
 import { getSession } from './auth'
 import { eq } from 'drizzle-orm'
-import { cache } from 'react'
 import { issues, users } from '@/db/schema'
-import { mockDelay } from './utils'
+
+// Current user
+export const getCurrentUser = async () => {
+  const session = await getSession()
+  if (!session) return null
+
+  try {
+    const result = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.userId))
+
+    return result[0] || null
+  } catch (error) {
+    console.error('Error getting user by ID:', error)
+    return null
+  }
+}
+
+// Get user by email
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.query.users.findFirst({
+      where: eq(users.email, email),
+    })
+
+    return user || null
+  } catch (error) {
+    console.error('Error getting user by email:', error)
+    return null
+  }
+}
+
+export async function getIssues() {
+  try {
+    const result = await db.query.issues.findMany({
+      with: {
+        user: true,
+      },
+      orderBy: (issues, { desc }) => [desc(issues.createdAt)],
+    })
+    return result
+  } catch (error) {
+    console.error('Error fetching issues:', error)
+    throw new Error('Failed to fetch issues')
+  }
+}
